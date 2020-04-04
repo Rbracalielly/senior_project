efb6e63be0346c3deac08603afc675823bb75303
erolan <46430626+erolan@users.noreply.github.com>
2020-04-04 16:29:36 -0400

    Login via Server
    Updated usersignup.html and server.js to query the database for a user and password.

--- node/server.js
+++ node/server.js
@@ -3,6 +3,53 @@ var fs = require("fs");
 var mysql = require("mysql");
 var credentials = require("./credentials");
 var qs = require("querystring");
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/usersignup.html'));
+});
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM userInformation WHERE userEmail = ? AND userPassword = ?', [userEmail, userPassword], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = userEmail;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}
+			response.end();
+		});
+	} else {
+		response.send('Please enter email and Password!');
+		response.end();
+	}
+});
+app.get('/userinformationpage', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.userEmail + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+
+
+
 
 http.createServer(function(req, res) {
   try {
